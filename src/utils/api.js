06ee580922e03dc9b62697344ca07482957d25bc
Lance <149887891+lance-ui@users.noreply.github.com>
2025-10-20 09:13:25 -0700

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -58,4 +58,6 @@ export const downloadFile = (hash) => {
   window.open(`${API_BASE.replace('/api', '')}/download/${hash}`, '_blank');
 };
 
-export const getDownlink = hash => return `${API_BASE.replace('/api')}/download/${hash}`;
+export const getDownlink = (hash) => { 
+  return `${API_BASE.replace('/api')}/download/${hash}`;
+}
